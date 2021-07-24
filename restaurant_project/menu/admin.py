4df27045a4609d8ee0d70fbe1549e776f737664d
Artem <lukart80>
2021-07-24 22:39:00 +0300

    Модели Cuisine и Product зарегестрированы в админке

--- restaurant_project/menu/admin.py
+++ restaurant_project/menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cuisine, Product
 
-# Register your models here.
+
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    search_fields = ('name',)
+    list_filter = ('price', 'quantity',)
