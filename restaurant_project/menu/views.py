20c0db24c1fe918e86fe8dc86a99e3b43494e7c6
Artem <lukart80>
2021-07-24 22:46:26 +0300

    В функцию homepage_view добавлен пагинатор

--- restaurant_project/menu/views.py
+++ restaurant_project/menu/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
 
 
 def homepage_view(request):
     """View-функция для отображения главной страницы."""
-    return render(request, 'menu/homepage.html', {})
+    products = Product.objects.prefetch_related('cuisine').all()
+    paginator = Paginator(products, 9)
+    page = paginator.get_page(request.GET.get('page'))
+    context = {
+        'page': page,
+    }
+    return render(request, 'menu/homepage.html', context)
