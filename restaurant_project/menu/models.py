d78ba31df03e57253f75d505bd244a926a040c92
Artem <lukart80>
2021-07-24 22:39:21 +0300

    Добавлена строковая репрезентация для моделей

--- restaurant_project/menu/models.py
+++ restaurant_project/menu/models.py
@@ -3,10 +3,15 @@ from django.db import models
 
 
 class Cuisine(models.Model):
-    cuisine = models.CharField(verbose_name='кухня', max_length=100)
+    """Модель для названий кухнь."""
+    name = models.CharField(verbose_name='кухня', max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
+    """Модель названий продуктов."""
     name = models.CharField(verbose_name='Название продукта', max_length=100)
     price = models.DecimalField(
         verbose_name='Цена продукта',
@@ -14,9 +19,14 @@ class Product(models.Model):
         decimal_places=2,
         max_digits=30000,
     )
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(verbose_name='количество')
     cuisine = models.ForeignKey(Cuisine,
                                 on_delete=models.CASCADE,
-                                related_name='products')
-    picture = models.URLField()
-    description = models.TextField(validators=[MaxLengthValidator(3000)])
+                                related_name='products',
+                                verbose_name='кухня')
+    picture = models.URLField(verbose_name='картинка')
+    description = models.TextField(validators=[MaxLengthValidator(3000)],
+                                   verbose_name='описание')
+
+    def __str__(self):
+        return self.name
