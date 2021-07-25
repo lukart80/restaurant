ad075bdb82dd6d33981bba8be6caddcaaf90f70e
Artem <lukart80>
2021-07-25 09:56:19 +0300

    Добавлены URL для cart

--- restaurant_project/restaurant_project/urls.py
+++ restaurant_project/restaurant_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('menu.urls'),)
+    path('', include('menu.urls')),
+    path('cart/', include('cart.urls')),
 ]
