9afbf809ef2e5f98b3038bbfbebc96cc9940fcf2
Artem <lukart80>
2021-07-25 22:51:00 +0300

    Модели для разных видов заказов

--- restaurant_project/order/admin.py
+++ restaurant_project/order/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import DeliveryOrder, PickUpOrder, OrderItem
+
+admin.site.register(DeliveryOrder)
+admin.site.register(PickUpOrder)
+
+
+@admin.register(OrderItem)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('order',)
+
+
 
 
--- restaurant_project/order/models.py
+++ restaurant_project/order/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, \
+    GenericRelation
+from menu.models import Product
 
 STATUS_CHOICES_DELIVERY = [
     ('unpaid', 'неоплачен'),
@@ -22,21 +26,50 @@ RESTAURANT_CHOICES = [
 
 
 class BaseOrder(models.Model):
+    """Базовая модель заказа."""
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     code = models.IntegerField()
     payed = models.BooleanField(default=False)
+    products = GenericRelation('OrderItem',
+                               content_type_field='content_type',
+                               object_id_field='object_id'
+                               )
 
     class Meta:
         abstract = True
 
 
 class DeliveryOrder(BaseOrder):
-    status = models.CharField(choices=STATUS_CHOICES_DELIVERY)
+    """Модель для заказа с доствкой."""
+    status = models.CharField(choices=STATUS_CHOICES_DELIVERY, max_length=100)
     delivery_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'Доставка номер {self.id}'
 
 
 class PickUpOrder(BaseOrder):
-    status = models.CharField(choices=STATUS_CHOICES_PICKUP)
-    restaurant = models.CharField(choices=RESTAURANT_CHOICES)
+    """Модель для заказа самовывозом."""
+    status = models.CharField(choices=STATUS_CHOICES_PICKUP, max_length=100)
+    restaurant = models.CharField(choices=RESTAURANT_CHOICES, max_length=100)
+
+    def __str__(self):
+        return f'Самовывоз {self.id}'
+
+
+class OrderItem(models.Model):
+    """Модель для хранения заказанных продуктов."""
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+
+                                     )
+    object_id = models.PositiveIntegerField()
+    order = GenericForeignKey('content_type', 'object_id')
+    product = models.ForeignKey(Product,
+                                on_delete=models.CASCADE,
+                                related_name='orders')
+
+    def __str__(self):
+        return self.product.name
